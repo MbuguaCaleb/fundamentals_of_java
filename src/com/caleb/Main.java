2c5b51adb2b8da98ea89c5103593de8611ca618c
MbuguaCaleb <mbuguacaleb30@gmail.com>
2021-02-13 13:41:33 +0300

    beginning-java-fundamentals

---
+++ src/com/caleb/Main.java
@@ -0,0 +1,8 @@
+package com.caleb;
+
+public class Main {
+    public static void main(String[] args) {
+        //semi colon in java must be written the end of each line
+        System.out.println("Caleb's first Java Line of Code");
+    }
+}
