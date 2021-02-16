a1aa53b07b906f5da87bd9136af833ecc338e986
MbuguaCaleb <mbuguacaleb30@gmail.com>
2021-02-16 22:20:55 +0300

    primitive-and-reference-types

--- src/com/caleb/Main.java
+++ src/com/caleb/Main.java
@@ -1,7 +1,15 @@
 package com.caleb;
 
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-       System.out.println("Hello world!");
+        //reference types are copied by their references(pointers)
+        //primitive types are copied by their values
+        Point point1 = new Point( x:1,y:2);
+        Point point2 = point1;
+        point2.x =2;
+        System.out.println();
+
     }
 }
