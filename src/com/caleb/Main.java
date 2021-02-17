c3d85cad2e7d5ddfa6911fc9d93843bef37399e4
MbuguaCaleb <mbuguacaleb30@gmail.com>
2021-02-17 17:01:55 +0300

    loops-in-java

--- src/com/caleb/Main.java
+++ src/com/caleb/Main.java
@@ -1,7 +1,24 @@
 package com.caleb;
 
+import java.util.Scanner;
+
 public class Main {
+
+    //Once Java encounters the Break it leaves the loop and
+    //does not execute anything else
+
+    //Continue statement moves the Control to the beginning of a Loop
+
     public static void main(String[] args) {
-       System.out.println("Hello world!");
+
+        String [] fruits = {"Apple", "Mango","Orange"};
+        for (int i =0; i < fruits.length; i++){
+            System.out.println(fruits[i]);
+        }
+
+        for (String fruit: fruits){
+            System.out.println(fruit);
+        }
+
     }
 }
