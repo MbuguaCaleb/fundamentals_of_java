cb3c15535c62a9405a460063a5e1dff5ea4a8fb8
MbuguaCaleb <mbuguacaleb30@gmail.com>
2021-02-17 06:18:35 +0300

    strings

--- src/com/caleb/Main.java
+++ src/com/caleb/Main.java
@@ -1,15 +1,15 @@
 package com.caleb;
 
-import java.awt.*;
+
 
 public class Main {
     public static void main(String[] args) {
-        //reference types are copied by their references(pointers)
-        //primitive types are copied by their values
-        Point point1 = new Point( x:1,y:2);
-        Point point2 = point1;
-        point2.x =2;
-        System.out.println();
+       String hello ="   CALEB" + "MBUGUA!!  ";
+       //Concat strings
+        //String is a class therefore it has methods we canbe able to access.
+
+
+        System.out.println(hello.toLowerCase());
 
     }
 }
